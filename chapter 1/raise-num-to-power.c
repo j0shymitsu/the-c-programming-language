39178f4c3eb058d93b32d4f31a5af3bc7630e424
Josh Birch <2312292@chester.ac.uk>
2024-09-13 10:26:23 +0100

    More reading on chapter 1
    up to 1.7

---
+++ chapter 1/raise-num-to-power.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// NOTE: The standard library contains a function "pow(x, y)" that computes power. This is for demonstration
+
+int power(int m, int n);
+
+/* TEST POWER FUNCTION */
+int main(void)
+{
+    int i;
+
+    for(i = 0; i < 10; i++)
+    {
+        printf("%d %d %d\n", i, power(2, i), power(-3, i));
+    }
+
+    return 0;
+}
+
+/* FUNCTION DEFINITION */
+int power(int base, int n)    // Parameters are local to function
+{
+    int i, p;
+
+    p = 1;
+
+    for(i = 1; i <= n; i++)
+    {
+        p = p * base;
+    }
+
+    return p;
+}
